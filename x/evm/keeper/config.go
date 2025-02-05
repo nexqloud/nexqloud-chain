c69c6f043e1a4dd2d6153656992256c3b444dfdc
SaswataPatra <saswatapatra15@gmail.com>
2025-02-05 19:47:41 +0530

    `Update ContractAddress in EVMConfig`

--- x/evm/keeper/config.go
+++ x/evm/keeper/config.go
@@ -18,7 +18,7 @@ const (
 	NodeURL         = "http://localhost:8545"
 	PrivateKeyHex   = "E9B1D63E8ACD7FE676ACB43AFB390D4B0202DAB61ABEC9CF2A561E4BECB147DE"
 	ChainID         = 6000                                         // Change Chain ID if needed
-	ContractAddress = "0x7673cE09B30e758d68F0ab707e0DAf6e0E180748" // Replace with actual contract address
+	ContractAddress = "0xF8c4AbD2d2573eBDA51cc886A9b6882E435C80A2" // Replace with actual contract address
 )
 
 // EVMConfig creates the EVMConfig based on current state
