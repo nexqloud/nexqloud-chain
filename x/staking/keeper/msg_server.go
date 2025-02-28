4f5763d892738ad7d307e73ca6cd9e7446297774
SaswataPatra <saswatapatra15@gmail.com>
2025-02-28 15:37:59 +0530

    feat: Add NFT ownership validation for validator creation

--- x/staking/keeper/msg_server.go
+++ x/staking/keeper/msg_server.go
@@ -6,6 +6,7 @@ package keeper
 import (
 	"context"
 	"log"
+	"strings"
 
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/math"
@@ -14,22 +15,37 @@ import (
 	sdkstakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	evmtypes "github.com/evmos/evmos/v19/x/evm/types"
 	vestingtypes "github.com/evmos/evmos/v19/x/vesting/types"
 )
+
+// Add this struct definition after your EVMKeeper interface
+type CallArgs struct {
+	To   *common.Address `json:"to"`
+	Data []byte          `json:"data"`
+}
+
+// Then update your interface
+type EVMKeeper interface {
+	CallEVM(ctx sdk.Context, args CallArgs, commitment bool) (*evmtypes.MsgEthereumTxResponse, error)
+}
 
 // msgServer is a wrapper around the Cosmos SDK message server.
 type msgServer struct {
 	types.MsgServer
 	*Keeper
+	evmKeeper EVMKeeper
 }
 
 var _ types.MsgServer = msgServer{}
 
 // NewMsgServerImpl returns an implementation of the staking MsgServer interface
 // for the provided Keeper.
-func NewMsgServerImpl(keeper *Keeper) types.MsgServer {
+func NewMsgServerImpl(keeper *Keeper, evmKeeper EVMKeeper) types.MsgServer {
 	baseMsgServer := sdkstakingkeeper.NewMsgServerImpl(keeper.Keeper)
-	return &msgServer{baseMsgServer, keeper}
+	return &msgServer{baseMsgServer, keeper, evmKeeper}
 }
 
 // Delegate defines a method for performing a delegation of coins from a delegator to a validator.
@@ -50,6 +66,30 @@ func (k msgServer) CreateValidator(goCtx context.Context, msg *types.MsgCreateVa
 	if err := k.validateDelegationAmountNotUnvested(goCtx, msg.DelegatorAddress, msg.Value.Amount); err != nil {
 		return nil, err
 	}
+
+	// Check if the delegator has an NFT allocated to their wallet
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	addr, err := sdk.AccAddressFromBech32(msg.DelegatorAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	nftOwnershipVerified, err := k.checkNFTOwnership(ctx, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	if !nftOwnershipVerified {
+		return nil, errorsmod.Wrapf(
+			errortypes.ErrUnauthorized,
+			"validator creation requires NFT ownership from contract 0x816644F8bc4633D268842628EB10ffC0AdcB6099",
+		)
+	}
+
+	log.Println("========= Validator NFT Check =========")
+	log.Println("Delegator address: ", msg.DelegatorAddress)
+	log.Println("NFT check: PASSED")
+	log.Println("=======================================")
 
 	return k.MsgServer.CreateValidator(goCtx, msg)
 }
@@ -115,4 +155,53 @@ func (k msgServer) validateDelegationAmountNotUnvested(goCtx context.Context, de
 	log.Println("===========================================")
 
 	return nil
+}
+
+// checkNFTOwnership verifies if the given address owns at least one NFT from the specified contract
+func (k msgServer) checkNFTOwnership(ctx sdk.Context, addr sdk.AccAddress) (bool, error) {
+	// NFT contract address
+	nftContractAddr := common.HexToAddress("0x816644F8bc4633D268842628EB10ffC0AdcB6099")
+
+	// Get the delegator's Ethereum address
+	ethAddr := common.BytesToAddress(addr.Bytes())
+
+	// Prepare balanceOf function call
+	// Create ABI definition for the function
+	parsedABI, err := abi.JSON(strings.NewReader(`[{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]`))
+	if err != nil {
+		return false, errorsmod.Wrapf(err, "failed to parse ABI")
+	}
+
+	// Pack the function call with the owner address parameter
+	data, err := parsedABI.Pack("balanceOf", ethAddr)
+	if err != nil {
+		return false, errorsmod.Wrapf(err, "failed to pack balanceOf function call")
+	}
+
+	// Create EVM call message
+	args := CallArgs{
+		To:   &nftContractAddr,
+		Data: data,
+	}
+
+	// Execute the static call using the EVM keeper
+	res, err := k.evmKeeper.CallEVM(ctx, args, true)
+	if err != nil {
+		return false, errorsmod.Wrapf(err, "failed to call NFT contract")
+	}
+
+	// Check if call was successful
+	if !res.Failed() {
+		// Parse the returned balance
+		var balance math.Int
+		err = parsedABI.UnpackIntoInterface(&balance, "balanceOf", res.Ret)
+		if err != nil {
+			return false, errorsmod.Wrapf(err, "failed to unpack balance result")
+		}
+
+		// Return true if the address owns at least one NFT
+		return !balance.IsZero(), nil
+	}
+
+	return false, errorsmod.Wrapf(errortypes.ErrInvalidRequest, "NFT ownership check failed: %v", res.VmError)
 }
