976497dd1b4384aac87f988cafad896310075017
Saswata Patra <saswatapatra15@gmail.com>
2025-05-27 19:47:39 +0530

    chore: Update contract addresses in config.go for accuracy
    - Updated the OnlineServerCountContract, NFTContractAddress, and WalletStateContractAddress to reflect the correct configuration.
    - This change improves the accuracy of the staking module's contract interactions.

--- x/config/config.go
+++ x/config/config.go
@@ -3,9 +3,9 @@ package config
 // Contract addresses and other configuration constants for the staking module
 const (
 	ChainID         = 6000         
-	OnlineServerCountContract = "0x7020FFa1cA9dda4e028E9c4a5FF94E9f3E0C7fC8"
+	OnlineServerCountContract = "0x110B18CF36a9Dc5D39ded58e73Cc45443442BEEE"
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
-	NFTContractAddress = "0x80f077D4D0E46eED6277b02DcCF81baa099E1AF4"
+	NFTContractAddress = "0x30Be2eBfa75Cb17766E2CC0830c0FBD3FF7A05B4"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
-	WalletStateContractAddress = "0x8F9DEeCdA284Bc19d3d2892abfF1185e051CFd69"
+	WalletStateContractAddress = "0x2dc2B9a574d10D52759555B598130316D26Ef6f8"
 )
