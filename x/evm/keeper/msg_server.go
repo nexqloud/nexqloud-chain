69cec19f7e497a75e50052e9efae59d9dec37e37
Saswata Patra <saswatapatra15@gmail.com>
2025-06-02 16:47:32 +0530

    chore: Update whitelist configuration in msg_server.go for accuracy
    - Enabled the staging address in the whitelist for correct configuration.
    - Commented out the development address to prevent accidental usage in the staging environment.

--- x/evm/keeper/msg_server.go
+++ x/evm/keeper/msg_server.go
@@ -34,10 +34,10 @@ import (
 var _ types.MsgServer = &Keeper{}
 var whitelist = map[string]bool{
 	//staging config
-	// "0x0845ed4B7CE9c886BC801edaF4f31F5123ffE69A": true,
+	"0x0845ed4B7CE9c886BC801edaF4f31F5123ffE69A": true,
 
 	//dev config
-	"0x010f60950BbB583662Cd13175BdC10a60bB868F4": true,
+	// "0x010f60950BbB583662Cd13175BdC10a60bB868F4": true,
 }
 
 func getFunctionSelector(signature string) []byte {
