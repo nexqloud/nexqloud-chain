b6c50898ccc9eece794e8d3a19d936ad7a59000d
Saswata Patra <saswatapatra15@gmail.com>
2025-06-02 16:14:43 +0530

    chore: Update WalletStateContractAddress in config.go for accuracy
    - Replaced the existing WalletStateContractAddress with a new address to ensure correct configuration.
    - This change enhances the accuracy of the staking module's configuration constants.

--- x/config/config.go
+++ x/config/config.go
@@ -7,5 +7,5 @@ const (
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
 	NFTContractAddress = "0xD49adEb95b0Ebe2Ef6D0e3801D40485bf04444Ad"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
-	WalletStateContractAddress = "0x3eFe64DE2Fa121DEdf0EC12BE97C517E56467Dee"
+	WalletStateContractAddress = "0x35d83D0Dd21f5c7f5D3799b966256A0717e124F1"
 )
