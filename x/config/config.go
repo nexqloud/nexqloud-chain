0869af35622a4cf1a6e4959b29c229ff7d2f06bc
Saswata Patra <saswatapatra15@gmail.com>
2025-06-02 17:01:08 +0530

    chore: Update OnlineServerCountContract address in config.go for accuracy
    - Changed the OnlineServerCountContract address to ensure it reflects the correct configuration for the staking module.

--- x/config/config.go
+++ x/config/config.go
@@ -3,7 +3,7 @@ package config
 // Contract addresses and other configuration constants for the staking module
 const (
 	ChainID         = 6000         
-	OnlineServerCountContract = "0xE29BA5872550567f560c209Aec3b8EBEB52d5BE5"
+	OnlineServerCountContract = "0x57B45055116f8D34F11dB5e10F4f0c6F7013196e"
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
 	NFTContractAddress = "0xD49adEb95b0Ebe2Ef6D0e3801D40485bf04444Ad"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
