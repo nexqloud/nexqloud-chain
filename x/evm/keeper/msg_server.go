66def7531efe750334de9cedd8e155ae41b358e1
Saswata Patra <saswatapatra15@gmail.com>
2025-06-11 19:24:59 +0530

    chore: Update whitelist address in msg_server.go for configuration accuracy
    - Replaced the development address in the whitelist with a new address to ensure correct configuration.
    - This change enhances the accuracy of address management for different environments.

--- x/evm/keeper/msg_server.go
+++ x/evm/keeper/msg_server.go
@@ -37,7 +37,7 @@ var whitelist = map[string]bool{
 	// "0x0845ed4B7CE9c886BC801edaF4f31F5123ffE69A": true,
 
 	//dev config
-	"0x010f60950BbB583662Cd13175BdC10a60bB868F4": true,
+	"0xE56A21BB0619225616DE7613937b2b816A14deB1": true,
 }
 
 func getFunctionSelector(signature string) []byte {
