8a8efb5682ef2f229ee24dbb4239419d1f03d4a9
Romit Karmakar <rkarmakar@nexqloud.io>
2025-06-16 19:06:19 +0530

    Update config.go

--- x/config/config.go
+++ x/config/config.go
@@ -3,9 +3,9 @@ package config
 // Contract addresses and other configuration constants for the staking module
 const (
 	ChainID         = 6000         
-	OnlineServerCountContract = "0x3A4762f6c26C5309320D1ed4F726463Ba430bD8A"
+	OnlineServerCountContract = "0xb46b2e10bc95b55153eeb804Ec159214Cc50fc58"
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
-	NFTContractAddress = "0x1dC7fe6712F76c4ebFaad69284FbFe31153b9ebE"
+	NFTContractAddress = "0x33f9A2F68e04124D0D87C7dd2912B1486eE8997f"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
-	WalletStateContractAddress = "0x1bABb0B43e8E16Dc62C13a7d4678B290b0778C99"
+	WalletStateContractAddress = "0x0aDFa87015e3b115bC985F6E41002F1d7BE0BA64"
 )
