f783a19467c4f381a91913eab0d35784f3df89b7
Saswata Patra <saswatapatra15@gmail.com>
2025-06-27 18:41:30 +0530

    Update contract addresses in config.go for staking module

--- x/config/config.go
+++ x/config/config.go
@@ -3,9 +3,9 @@ package config
 // Contract addresses and other configuration constants for the staking module
 const (
 	ChainID         = 6000         
-	OnlineServerCountContract = "0x187309f5b85f46aCE09e6E294532B28b2f2224fD"
+	OnlineServerCountContract = "0xA99a5526C8367deaB9313823E4159Acf132a336a"
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
-	NFTContractAddress = "0xb46b2e10bc95b55153eeb804Ec159214Cc50fc58"
+	NFTContractAddress = "0x5414DAF72fCB204C032Ac4AE7f9C931a9bc9b3Df"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
-	WalletStateContractAddress = "0x9078f807f5049e2C74ff50269dec960DBD65824e"
+	WalletStateContractAddress = "0x111d505c506d16B79D5aE9A5c7471c1cA0b71BbD"
 )
