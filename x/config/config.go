e10c54971891ccee2b2826688aea84be07ca0a2b
Saswata Patra <saswatapatra15@gmail.com>
2025-10-29 08:44:25 +0400

    Update OnlineServerCountContract comment in config.go for staging clarity
    - Commented out the previous OnlineServerCountContract address and clarified the current address status for staging.
    - This change enhances understanding of the contract's operational state in the staging environment.

--- x/config/config.go
+++ x/config/config.go
@@ -3,7 +3,8 @@ package config
 // Contract addresses and other configuration constants for the staking module
 const (
 	ChainID                   = 6000
-	OnlineServerCountContract = "0xaA51C7e32dA1266447909b6C40772276A43453e8"
+	// OnlineServerCountContract = "0xaA51C7e32dA1266447909b6C40772276A43453e8" //closed in staging
+	OnlineServerCountContract = "0xaA51C7e32dA1266447909b6C40772276A43453e8" //open in staging
 	// NFTContractAddress is the address of the NFT contract that validators must own tokens from
 	NFTContractAddress = "0x1c09B13B4Ea885CAF8b07a69231Ace3B0cCd91Bf"
 	// WalletStateContractAddress is the address of the contract that stores validator requirements
